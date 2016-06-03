e95b8bcc47ab8904e799efef37a7e2528b11ed42
DomCristaldi <domeniccristaldi@gmail.com>
2016-06-03 04:39:34 -0400

    Wrote a static function to be called from command line to build players

---
+++ TestProject/Assets/Scripts/BuildPipeline/Editor/BuildTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEditor;
+
+public static class BuildTool {
+
+    public static void BuildStandaloneGame() {
+        string[] args = Environment.GetCommandLineArgs();
+
+        Debug.Log("-------Command Line args-------\n");
+        foreach (string s in args) {
+            Debug.Log(s);
+        }
+        Debug.Log("---------------------------------");
+
+    }
+
+
+
+}
