47fee83306625c74d188ddaad4087874dcaaacc0
DomCristaldi <domeniccristaldi@gmail.com>
2016-06-03 05:53:47 -0400

    Refined build argumetnts to take a user-supplied name

--- TestProject/Assets/Scripts/BuildPipeline/Editor/BuildTool.cs
+++ TestProject/Assets/Scripts/BuildPipeline/Editor/BuildTool.cs
@@ -10,18 +10,22 @@ public static class BuildTool {
 
         string[] scenesToBuild = new string[] { "Assets/Scenes/test.unity" };
         string buildToLocation = "";
+        string projectName = "";
 
 
+        //PARSE THE COMMAND LINE ARGUMENTS FOR BUILD-UNIQUE INFO
         for (int i = 0; i < args.Length; ++i) {
             if (args[i] == "BuildTool.BuildStandaloneGame") {
                 buildToLocation = args[i + 1];
+                projectName = args[i + 2];
             }
         }
 
+        //ACUTALLY MAKE THE BUILD
         BuildPipeline.BuildPlayer(scenesToBuild,
-                                  buildToLocation,
+                                  buildToLocation, //+ "" + projectName + ".exe",
                                   BuildTarget.StandaloneWindows64,
-                                  BuildOptions.None);
+                                  BuildOptions.Il2CPP);
 
     }
 
